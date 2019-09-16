20e85d21355bb2a106c9772444ff867686bce81f
Shuxian Wang <wsx@udscbt.io>
2019-09-16 12:59:42 -0700

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+use hog::{Game, PureStrategy, State, ImpureStrategy};
+
+fn main() {
+    let mut final_strategy = PureStrategy::import("final.strat").unwrap();
+    let mut new_strategy = PureStrategy::import("final.strat").unwrap();
+    let ultimate_strategy = ImpureStrategy::build();
+//    print!("{}", Game::new().win_rate(&new_strategy, &final_strategy));
+//    new_strategy.improve(&ultimate_strategy);
+}
