ad5f9c6e9c5f266f0fc9725df8bb06e062ca4d90
pixelled <pixelledliu@gmail.com>
2019-09-22 18:47:06 +0000

    Some update

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use hog::{Game, PureStrategy, State, ImpureStrategy};
 
 fn main() {
-    let mut final_strategy = PureStrategy::import("final.strat").unwrap();
-    let mut new_strategy = PureStrategy::import("final.strat").unwrap();
+    // let mut final_strategy = PureStrategy::import("final.strat").unwrap();
+    let mut new_strategy = PureStrategy::import("learn.strat").unwrap();
     let ultimate_strategy = ImpureStrategy::build();
 //    print!("{}", Game::new().win_rate(&new_strategy, &final_strategy));
-//    new_strategy.improve(&ultimate_strategy);
+    new_strategy.improve(&ultimate_strategy);
+    new_strategy.export("learn.strat");
 }
