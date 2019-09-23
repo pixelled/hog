e576ee3e241e112f8040d025ba52e4d889ba019e
pixelled <pixelledliu@gmail.com>
2019-09-23 11:09:43 -0700

    play against ultimate_strategy

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,6 @@ fn main() {
     let mut new_strategy = PureStrategy::import("learn.strat").unwrap();
     let ultimate_strategy = ImpureStrategy::build();
 //    print!("{}", Game::new().win_rate(&new_strategy, &final_strategy));
-	for _ in 0..2 {
-		new_strategy.improve(&new_strategy.clone());
-	}
-	for _ in 0..5 {
-		new_strategy.improve(&ultimate_strategy)
-	}
+    new_strategy.improve(&ultimate_strategy);
     new_strategy.export("learn.strat");
 }
